bc69bfd178d2ae17a856610e97ccb27a5f65817d
mzimr <schwimm-max@web.de>
2026-06-30 14:20:06 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,29 @@
-self.addEventListener('install',e=>self.skipWaiting());self.addEventListener('fetch',()=>{});
+const CACHE="zuletzt-v1";
+
+const FILES=[
+"/",
+"index.html",
+"styles.css",
+"app.js",
+"manifest.json"
+];
+
+self.addEventListener("install",e=>{
+
+e.waitUntil(
+
+caches.open(CACHE).then(cache=>cache.addAll(FILES))
+
+);
+
+});
+
+self.addEventListener("fetch",e=>{
+
+e.respondWith(
+
+caches.match(e.request).then(r=>r||fetch(e.request))
+
+);
+
+});
